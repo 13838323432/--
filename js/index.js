72cabb4eff96aabb84c4661b24184b510467c5c5
13838323432 <50703126+13838323432@users.noreply.github.com>
2019-05-30 16:19:36 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,4 +7,10 @@
 $(".attention h1").on('click', function () {
     $(this).toggleClass("attention_");
     $(this).siblings('h1').removeClass("attention_");
-      })  
+})  
+$('.right li').eq(0).on('click', function () {
+          window.location.href="my.html"
+      })
+$('.picture').on('click', function(){
+    window.location.href = 'list.html';
+})
